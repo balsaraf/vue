b082a8054600e8e821d9bf675edf266b4b527c5e
balsaraf <ashabalsaraf10@gmail.com>
2017-04-25 13:12:22 +0530

    Update build.js
    adding files

--- build/build.js
+++ build/build.js
@@ -1,4 +1,8 @@
+// https://github.com/shelljs/shelljs
+require('./check-versions')()
+
 ()process.env.NODE_ENV = 'production'
+
 var ora = require('ora')
 var path = require('path')
 var chalk = require('chalk')
